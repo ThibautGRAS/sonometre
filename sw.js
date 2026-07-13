3e74ee88a18be57f2699d0ea0662e0f84202207d
ThibautGRAS <39792145+ThibautGRAS@users.noreply.github.com>
2026-07-13 18:04:45 +0300

    V31.1 - SW build 32b2bb45

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rend l'application ouvrable 100 % hors ligne.
    La version est estampillée à chaque déploiement (voir CACHE) : au changement,
    l'ancien cache est purgé et les fichiers re-téléchargés dès qu'un réseau est disponible. */
-const CACHE = 'sono-b9b11488';
+const CACHE = 'sono-32b2bb45';
 const ASSETS = [
   './',
   './index.html',
