50b6e29de35942bd600126ffabd02de616131a1b
ThibautGRAS <39792145+ThibautGRAS@users.noreply.github.com>
2026-07-13 23:21:41 +0300

    V35.13 SW build e0bc8801

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rend l'application ouvrable 100 % hors ligne.
    La version est estampillée à chaque déploiement (voir CACHE) : au changement,
    l'ancien cache est purgé et les fichiers re-téléchargés dès qu'un réseau est disponible. */
-const CACHE = 'sono-d230b8ff';
+const CACHE = 'sono-e0bc8801';
 const ASSETS = [
   './',
   './index.html',
