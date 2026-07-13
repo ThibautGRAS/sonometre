a817e90150cca323cf34e965091d1fcf47d70b2c
ThibautGRAS <39792145+ThibautGRAS@users.noreply.github.com>
2026-07-13 13:44:16 +0300

    V30.2 - SW build f2dce513

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rend l'application ouvrable 100 % hors ligne.
    La version est estampillée à chaque déploiement (voir CACHE) : au changement,
    l'ancien cache est purgé et les fichiers re-téléchargés dès qu'un réseau est disponible. */
-const CACHE = 'sono-fd189533';
+const CACHE = 'sono-f2dce513';
 const ASSETS = [
   './',
   './index.html',
