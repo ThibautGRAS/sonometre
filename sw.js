b4c8058ec609a281b19bf1fedea4caf178846ec2
ThibautGRAS <39792145+ThibautGRAS@users.noreply.github.com>
2026-07-13 17:51:11 +0300

    V30.4 - SW build ba05ceba

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rend l'application ouvrable 100 % hors ligne.
    La version est estampillée à chaque déploiement (voir CACHE) : au changement,
    l'ancien cache est purgé et les fichiers re-téléchargés dès qu'un réseau est disponible. */
-const CACHE = 'sono-0ec8388b';
+const CACHE = 'sono-ba05ceba';
 const ASSETS = [
   './',
   './index.html',
