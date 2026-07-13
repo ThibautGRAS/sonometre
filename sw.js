111e55ac2b0e3b1e5c7bf28d0f6028af4c597100
ThibautGRAS <39792145+ThibautGRAS@users.noreply.github.com>
2026-07-13 13:23:54 +0300

    V29.6 - SW build 4d80be5f

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rend l'application ouvrable 100 % hors ligne.
    La version est estampillée à chaque déploiement (voir CACHE) : au changement,
    l'ancien cache est purgé et les fichiers re-téléchargés dès qu'un réseau est disponible. */
-const CACHE = 'sono-281c8cb4';
+const CACHE = 'sono-4d80be5f';
 const ASSETS = [
   './',
   './index.html',
