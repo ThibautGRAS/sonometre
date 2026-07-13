2936e30cade62aabafe707d09084b0d87599d360
ThibautGRAS <39792145+ThibautGRAS@users.noreply.github.com>
2026-07-13 20:16:48 +0300

    V35.3 - SW build 1ca950d0

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rend l'application ouvrable 100 % hors ligne.
    La version est estampillée à chaque déploiement (voir CACHE) : au changement,
    l'ancien cache est purgé et les fichiers re-téléchargés dès qu'un réseau est disponible. */
-const CACHE = 'sono-90727a2e';
+const CACHE = 'sono-1ca950d0';
 const ASSETS = [
   './',
   './index.html',
