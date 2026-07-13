0455504abb16aca4cccc9970e6748eaeeb40d623
ThibautGRAS <39792145+ThibautGRAS@users.noreply.github.com>
2026-07-13 19:04:09 +0300

    V33.1 - SW build d6375733

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Rend l'application ouvrable 100 % hors ligne.
    La version est estampillée à chaque déploiement (voir CACHE) : au changement,
    l'ancien cache est purgé et les fichiers re-téléchargés dès qu'un réseau est disponible. */
-const CACHE = 'sono-8a9ced71';
+const CACHE = 'sono-d6375733';
 const ASSETS = [
   './',
   './index.html',
